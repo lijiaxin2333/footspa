0e40439d11035a615296f2d152a92bafa72c02b4
lijiaxin.2333 <lijiaxin.2333@bytedance.com>
2025-11-05 23:34:59 +0800

    Initial Commit

---
+++ footspa/footspaApp.swift
@@ -0,0 +1,17 @@
+//
+//  footspaApp.swift
+//  footspa
+//
+//  Created by ljx on 2025/11/5.
+//
+
+import SwiftUI
+
+@main
+struct footspaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
