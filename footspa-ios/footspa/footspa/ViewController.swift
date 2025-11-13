7e5d85fcd451e03a9121220d22395a0cd3f6e8c2
lijiaxin.2333 <lijiaxin.2333@bytedance.com>
2025-11-13 11:20:09 +0800

    feat: 架构切swift uikit & swiftui 更加灵活

---
+++ footspa-ios/footspa/footspa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  footspa
+//
+//  Created by ljx on 2025/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
